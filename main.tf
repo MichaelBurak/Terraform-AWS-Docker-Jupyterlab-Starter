0c81cc3065123444d8e123d0cde0a49ade995e13
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-10-29 21:07:23 -0600

    Cleaning up and commenting main tf file

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ provider "aws" {
 resource "aws_key_pair" "ubuntu_jupyterlab" {
   key_name = "ubuntu_jupyterlab"
   # You will need to ssh-keygen a keypair by name of 'key' in the same directory as the .tf files 
-  # (think of ways of automating this process?)
   public_key = file("key.pub")
 }
 
@@ -26,7 +25,7 @@ resource "aws_security_group" "ubuntu_jupyterlab" {
   name        = "ubuntu_jupyterlab_security-group"
   description = "Allow SSH, HTTPS, HTTP and Jupyter Lab traffic"
 
-  # ingress should be more limited in any non-testing capacity
+  # allows SSH on port 22, in production would lock down to an IP (range)
 
   ingress {
     description = "SSH"
@@ -35,22 +34,6 @@ resource "aws_security_group" "ubuntu_jupyterlab" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # ingress {
-  #   description = "HTTPS"
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # ingress {
-  #   description = "HTTP"
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
 
   # should be in NACL later and shut down insecure ports -- https://docs.aws.amazon.com/vpc/latest/userguide/vpc-network-acls.html#nacl-ephemeral-ports  
   ingress {
@@ -61,7 +44,7 @@ resource "aws_security_group" "ubuntu_jupyterlab" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # for jupyter lab, this can remain but preferably change to your own IP 
+  # for jupyter lab, this can remain but preferably change to your own IP (range) 
   ingress {
     description = "HTTP"
     from_port   = 8888
@@ -84,7 +67,7 @@ resource "aws_security_group" "ubuntu_jupyterlab" {
 }
 
 resource "aws_instance" "ubuntu_jupyterlab" {
-  # Using the base free tier Amazon Linux 2 AMI
+  # Using the base free tier Amazon Linux 2 AMI currently
   ami           = "ami-013a129d325529d4d"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.ubuntu_jupyterlab.key_name
@@ -99,11 +82,4 @@ resource "aws_instance" "ubuntu_jupyterlab" {
     Name = "Terraform"
   }
 }
-
-
-# would add an elastic IP, may be useful
-# resource "aws_eip" "ubuntu_jupyterlab" {
-#   vpc      = true
-#   instance = aws_instance.ubuntu_jupyterlab.id
-# }
 
