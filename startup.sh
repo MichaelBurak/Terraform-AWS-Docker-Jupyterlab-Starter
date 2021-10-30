1bb29691b906097aa8586ee416e80599b3714fb4
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-10-29 19:59:38 -0600

    Add JUPYTER_TOKEN env var to docker run in startup script

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 sudo yum update
 sudo yum -y install docker 
 sudo systemctl start docker
-sudo docker run -d -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes jupyter/base-notebook
+sudo docker run --name my-jupyter-lab -d -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e JUPYTER_TOKEN="easy" jupyter/base-notebook
