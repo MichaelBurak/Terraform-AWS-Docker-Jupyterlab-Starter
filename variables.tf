ef3f792967bd9d19ba28838d89b53d178411bda1
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-11-02 21:33:51 -0600

    Add AZ variable

--- variables.tf
+++ variables.tf
@@ -5,6 +5,11 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
+variable "availability_zone" {
+  description = "AWS Availability Zone for resources"
+  type        = string
+  default     = "us-west-2a"
+}
 variable "resource_tags" {
   description = "Universal tags across resources"
   type        = map(string)
