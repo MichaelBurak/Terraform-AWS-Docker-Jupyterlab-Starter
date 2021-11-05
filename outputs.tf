beed61e4d4174df275e3a0afe5a104c3cf6be561
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-11-05 07:34:01 -0600

    Add S3 bucket provisioning

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+# Output variables
+
 output "ec2-public-dns" {
   value = aws_instance.docker-jupyterlab.public_dns
+}
+
+output "s3-bucket-domain" {
+  value = aws_s3_bucket.docker-jupyterlab.bucket_domain_name
 }
--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "docker-jupyterlab" {
-  bucket = "s3-website-test.hashicorp.com"
-  acl    = "public-read"
+  #   bucket = "s3-website-test.hashicorp.com" --> now just assigns and outputs random
+  # leaving it on public read for access from instance until IAM is set up
+  acl = "public-read"
 }
