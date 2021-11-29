410b749f0c7799026a2e96b44493c3c1db314ed1
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-11-29 14:59:14 -0700

    Remove comment after implementing random bucket url

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,6 @@ resource "random_pet" "s3prefix" {
 }
 
 resource "aws_s3_bucket" "docker-jupyterlab" {
-  #   bucket = "s3-website-test.hashicorp.com" --> now just assigns and outputs random
   bucket = "${random_pet.s3prefix.id}-jupyter-bucket"
   # leaving it on public read for access from instance until IAM is set up
   acl = "public-read"
